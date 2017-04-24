f8ef27aedc9e4c34aa273e1a67bd5bdf29a02598
Misha Rodygin <misha@abcsolute.com>
2017-04-24 18:55:21 +0300

    observer component

---
+++ src/components/Observer/Observer.js
@@ -0,0 +1,87 @@
+import React, { Component, PropTypes, Children } from 'react';
+import ReactDOM from 'react-dom';
+import { throttle } from 'utils/helpers';
+
+export default class Observer extends Component {
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(React.PropTypes.node),
+      PropTypes.node
+    ]).isRequired,
+    onMeasure: PropTypes.func,
+    container: PropTypes.string,
+    throttle: PropTypes.number
+  }
+
+  static defaultProps = {
+    container: 'div',
+    onMeasure: () => {},
+    throttle: 250
+  }
+
+  constructor(props) {
+    super(props);
+    this.node = null;
+    this.observer = null;
+    this.measure = throttle(this.measure, props.throttle);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.node = ReactDOM.findDOMNode(this);
+    if (this.node) {
+      this.initListeners();
+    }
+  }
+
+  componentWillUnmount() {
+    this.removeListeners();
+  }
+
+  initListeners() {
+    window.addEventListener('resize', this.measure);
+    window.addEventListener('scroll', this.measure);
+
+    const observer_options = {
+      attributes: true,
+      childList: true,
+      characterData: true,
+      subtree: true
+    };
+    this.observer = new MutationObserver(this.measure);
+    this.observer.observe(this.node, observer_options);
+  }
+
+  removeListeners() {
+    window.removeEventListener('resize', this.measure);
+    window.removeEventListener('scroll', this.measure);
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
+  measure = () => {
+    const rect = this.node.getBoundingClientRect();
+    this.handleMeasure({
+      top: rect.top,
+      bottom: rect.bottom,
+      left: rect.left,
+      right: rect.right,
+      width: rect.right - rect.left,
+      height: rect.bottom - rect.top
+    });
+  }
+
+  handleMeasure(value) {
+    const new_value = JSON.stringify(value);
+    if (this.last_value !== new_value) {
+      this.props.onMeasure(value);
+      this.last_value = new_value;
+    }
+  }
+
+  render() {
+    const { children, container: Container } = this.props;
+    return Children.count(children) === 1 ? children : <Container>{children}</Container>;
+  }
+}
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -87,3 +87,31 @@ export function getDeviceType() {
   }
   return 'desktop';
 }
+
+/*eslint-disable */
+export function throttle(func, ms) {
+
+  var isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+    func.apply(this, arguments);
+    isThrottled = true;
+    setTimeout(function() {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+/*eslint-enable */
